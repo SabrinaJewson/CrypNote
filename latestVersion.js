a020c49b686e3919ac8197710c66773f902c185c
KaiJewson <kai.jewson@gmail.com>
2021-11-09 20:15:23 +0000

    Release v1.0.0

--- latestVersion.js
+++ latestVersion.js
@@ -1,4 +1,4 @@
 // This file is imported with a <script> tag through a CDN to allow CrypNote to check what the
 // latest version is. We can't query GitHub directly through its API or anything because CORS
 // disallows `file://` URLs from sending requests anywhere.
-latestVersion = "0.1.0";
+latestVersion = "1.0.0";
